325d08a23e8b0e74cca3646f1ddf789edee31af0
Julia Baritz <jbaritz@gmail.com>
2015-01-28 14:48:47 -0600

    foundation, posts controller and routes

--- blogapp/app/controllers/users_controller.rb
+++ blogapp/app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
 class UsersController < ApplicationController
-  def 
+  def show
+    
+  end
 end
--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -1,23 +1,33 @@
 Rails.application.routes.draw do
   root 'welcomes#index'
   devise_for :users
-#               Prefix     Verb   URI Pattern                    Controller#Action
-#         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
-#            user_password POST   /users/password(.:format)      devise/passwords#create
-#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
-#                          PATCH  /users/password(.:format)      devise/passwords#update
-#                          PUT    /users/password(.:format)      devise/passwords#update
-# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
-#        user_registration POST   /users(.:format)               devise/registrations#create
-#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
-#                          PATCH  /users(.:format)               devise/registrations#update
-#                          PUT    /users(.:format)               devise/registrations#update
-#                          DELETE /users(.:format)               devise/registrations#destroy
-#                     root GET    /   
+      #               Prefix     Verb   URI Pattern                    Controller#Action
+      #         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+      #             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+      #     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+      #            user_password POST   /users/password(.:format)      devise/passwords#create
+      #        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+      #       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+      #                          PATCH  /users/password(.:format)      devise/passwords#update
+      #                          PUT    /users/password(.:format)      devise/passwords#update
+      # cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+      #        user_registration POST   /users(.:format)               devise/registrations#create
+      #    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+      #   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+      #                          PATCH  /users(.:format)               devise/registrations#update
+      #                          PUT    /users(.:format)               devise/registrations#update
+      #                          DELETE /users(.:format)               devise/registrations#destroy
+      #                     root GET    /   
+
+  get '/new-text-post', to: 'posts#new_text_post'
+  get '/new-picture-post', to: 'posts#new_picture_post'
+  get '/new-video-post', to: 'posts#new_video_post'
+  get '/new-audio-post', to: 'posts#new_audio_post'
+
+  post '/new-text-post', to: 'posts#submit_text_post'
+  post '/new-picture-post', to: 'posts#submit_picture_post'
+  post '/new-video-post', to: 'posts#submit_video_post'
+  post '/new-audio-post', to: 'posts#submit_audio_post'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
