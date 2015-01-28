f642dd6087e61ab3791f0a9c945e84b2703e3f60
Julia Baritz <jbaritz@gmail.com>
2015-01-28 10:39:33 -0600

    seeded

--- blogapp/db/seeds.rb
+++ blogapp/db/seeds.rb
@@ -7,103 +7,103 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# User.create!([
-#   {
-#     username: "John",
-#     password: "John1234",
-#     password_confirmation: "John1234",
-#     email: "John@john.com"
+User.create!([
+  {
+    username: "John",
+    password: "John1234",
+    password_confirmation: "John1234",
+    email: "John@john.com"
 
-#   },
-#   {
-#     username: "Mike",
-#     password: "Mike1234",
-#     password_confirmation: "Mike1234",
-#     email: "Mike@mike.com"
-#   },
-#   {
-#     username: "Sarah",
-#     password: "Sarah1234",
-#     password_confirmation: "Sarah1234",
-#     email: "Sarah@sarah.com"
-#   }
-#   ])
-# UserTextPost.create!([
-#   {
-#     title: "Hi",
-#     content: "This is a post!",
-#     tags: "Greetings",
-#     user_id: 1
-#   },
-#   {
-#     title: "Hello",
-#     content: "This is also a post!",
-#     tags: "Greetings",
-#     user_id: 2
-#   },
-#   {
-#     title: "Bye",
-#     content: "I sure love posting",
-#     tags: "bye",
-#     user_id: 3
-#   }
-
-
-#   ])
-
-# UserVideoPost.create!([
-#   {
-#     title: "Funny cats",
-#     content: "These cats are hilarious!",
-#     video_url: "www.funnycats.com",
-#     user_id: 1,
-#     tags: "Animals"
-#   },
-#   {
-#     title: "Funny dogs",
-#     content: "These dogs are hilarious!",
-#     video_url: "www.funnydogs.com",
-#     user_id: 2,
-#     tags: "Animals"
-#   },
-#   {
-#     title: "Awesome catches!",
-#     content: "These catches are awesome!",
-#     video_url: "www.football.com",
-#     user_id: 3,
-#     tags: "Sports"
-#   }
+  },
+  {
+    username: "Mike",
+    password: "Mike1234",
+    password_confirmation: "Mike1234",
+    email: "Mike@mike.com"
+  },
+  {
+    username: "Sarah",
+    password: "Sarah1234",
+    password_confirmation: "Sarah1234",
+    email: "Sarah@sarah.com"
+  }
+  ])
+UserTextPost.create!([
+  {
+    title: "Hi",
+    content: "This is a post!",
+    tags: "Greetings",
+    user_id: 1
+  },
+  {
+    title: "Hello",
+    content: "This is also a post!",
+    tags: "Greetings",
+    user_id: 2
+  },
+  {
+    title: "Bye",
+    content: "I sure love posting",
+    tags: "bye",
+    user_id: 3
+  }
 
 
+  ])
 
-#   ])
-
-# UserPicturePost.create!([
-#   {
-#     title: "Fish",
-#     content: "Check out this fish",
-#     image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRuBTUhARJ8dJJ4I1oNZVFbH80H1QXZTufrhhobs4Go6jZJk7MNcLQHBc",
-#     user_id: 1,
-#     tags: "Animals"
-#   },
-#   {
-#     title: "Building",
-#     content: "Check out this Building",
-#     image_url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTSAWF0PXHmmwM2f98HSUyEhXYpiwAVaGbHFPY343aCx1jM4p3PCOZIUXo",
-#     user_id: 2,
-#     tags: "architecture"
-#   },
-#   {
-#     title: "River",
-#     content: "Check out this River",
-#     image_url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkcgR02wcIk_tC7hjmcgQ5yFdf4eoQEln8zuGZ6Qyn0mJOL5lvMmKak5J7PQ",
-#     user_id: 3,
-#     tags: "Water"
-#   }
+UserVideoPost.create!([
+  {
+    title: "Funny cats",
+    content: "These cats are hilarious!",
+    video_url: "www.funnycats.com",
+    user_id: 1,
+    tags: "Animals"
+  },
+  {
+    title: "Funny dogs",
+    content: "These dogs are hilarious!",
+    video_url: "www.funnydogs.com",
+    user_id: 2,
+    tags: "Animals"
+  },
+  {
+    title: "Awesome catches!",
+    content: "These catches are awesome!",
+    video_url: "www.football.com",
+    user_id: 3,
+    tags: "Sports"
+  }
 
 
 
-#   ])
+  ])
+
+UserPicturePost.create!([
+  {
+    title: "Fish",
+    content: "Check out this fish",
+    image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRuBTUhARJ8dJJ4I1oNZVFbH80H1QXZTufrhhobs4Go6jZJk7MNcLQHBc",
+    user_id: 1,
+    tags: "Animals"
+  },
+  {
+    title: "Building",
+    content: "Check out this Building",
+    image_url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTSAWF0PXHmmwM2f98HSUyEhXYpiwAVaGbHFPY343aCx1jM4p3PCOZIUXo",
+    user_id: 2,
+    tags: "architecture"
+  },
+  {
+    title: "River",
+    content: "Check out this River",
+    image_url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkcgR02wcIk_tC7hjmcgQ5yFdf4eoQEln8zuGZ6Qyn0mJOL5lvMmKak5J7PQ",
+    user_id: 3,
+    tags: "Water"
+  }
+
+
+
+  ])
 
 UserAudioPost.create!([
   {
