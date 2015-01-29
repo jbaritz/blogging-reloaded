d76a09a9cbb2d5288d21d832c1d177095058e13e
Julia Baritz <jbaritz@gmail.com>
2015-01-29 10:24:13 -0600

    new text  form works

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -3,14 +3,32 @@ class PostsController < ApplicationController
     #view  form
   end
 
+  def new_picture_post
+    #view  form
+  end
+
   def submit_text_post
-    UserTextPost.new(text_params)
+    params['user_id'] = current_user.id
+    # puts "PARAMS:" 
+    # puts params
+    UserTextPost.create!(text_params)
+    redirect_to "/"
+  end
+
+  def submit_picture_post
+    params['user_id'] = current_user.id
+    UserPicturePost.create!(pic_params)
   end
 
   private
   def text_params
-     params.require(:post).permit(:title, :content, :tags)
+     params.require(:user_id)
+     params.permit(:title, :content, :tags, :user_id)
   end
 
+  def pic_params
+     params.require(:user_id, :image_url)
+     params.permit(:title, :content, :image_url, :user_id)
+  end
 end
 
