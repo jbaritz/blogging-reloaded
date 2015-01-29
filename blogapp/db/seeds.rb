b0d1135cfbce655642d7ebbe2111ce72ea6fdecf
James Knepper <knepperjb@gmail.com>
2015-01-29 22:43:59 +0000

    re-seed database due to new models

--- blogapp/db/seeds.rb
+++ blogapp/db/seeds.rb
@@ -7,130 +7,73 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create!([
-  {
-    username: "John",
-    password: "John1234",
-    password_confirmation: "John1234",
-    email: "John@john.com"
+# User.create!([
+#   {
+#     username: "John",
+#     password: "John1234",
+#     password_confirmation: "John1234",
+#     email: "John@john.com"
 
-  },
-  {
-    username: "Mike",
-    password: "Mike1234",
-    password_confirmation: "Mike1234",
-    email: "Mike@mike.com"
-  },
-  {
-    username: "Sarah",
-    password: "Sarah1234",
-    password_confirmation: "Sarah1234",
-    email: "Sarah@sarah.com"
-  }
-  ])
-UserTextPost.create!([
-  {
-    title: "Hi",
-    content: "This is a post!",
-    tags: "Greetings",
-    user_id: 1
-  },
-  {
-    title: "Hello",
-    content: "This is also a post!",
-    tags: "Greetings",
-    user_id: 2
-  },
-  {
-    title: "Bye",
-    content: "I sure love posting",
-    tags: "bye",
-    user_id: 3
-  }
+#   },
+#   {
+#     username: "Mike",
+#     password: "Mike1234",
+#     password_confirmation: "Mike1234",
+#     email: "Mike@mike.com"
+#   },
+#   {
+#     username: "Sarah",
+#     password: "Sarah1234",
+#     password_confirmation: "Sarah1234",
+#     email: "Sarah@sarah.com"
+#   }
+#   ])
 
-
-  ])
-
-UserVideoPost.create!([
+Post.create([
   {
-    title: "Funny cats",
-    content: "These cats are hilarious!",
-    video_url: "www.funnycats.com",
     user_id: 1,
-    tags: "Animals"
+    content: "This is a post about cars",
+    title: "cars",
+    post_type: "video"
   },
   {
-    title: "Funny dogs",
-    content: "These dogs are hilarious!",
-    video_url: "www.funnydogs.com",
     user_id: 2,
-    tags: "Animals"
+    content: "This is a post about boats",
+    title: "Boats",
+    post_type: "image"
   },
   {
-    title: "Awesome catches!",
-    content: "These catches are awesome!",
-    video_url: "www.football.com",
-    user_id: 3,
-    tags: "Sports"
-  }
-
-
-
-  ])
-
-UserPicturePost.create!([
-  {
-    title: "Fish",
-    content: "Check out this fish",
-    image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRuBTUhARJ8dJJ4I1oNZVFbH80H1QXZTufrhhobs4Go6jZJk7MNcLQHBc",
     user_id: 1,
-    tags: "Animals"
+    content: "I am having such a wonderful day",
+    title: "My day",
+    post_type: "text"
+
   },
   {
-    title: "Building",
-    content: "Check out this Building",
-    image_url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTSAWF0PXHmmwM2f98HSUyEhXYpiwAVaGbHFPY343aCx1jM4p3PCOZIUXo",
     user_id: 2,
-    tags: "architecture"
-  },
-  {
-    title: "River",
-    content: "Check out this River",
-    image_url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkcgR02wcIk_tC7hjmcgQ5yFdf4eoQEln8zuGZ6Qyn0mJOL5lvMmKak5J7PQ",
-    user_id: 3,
-    tags: "Water"
-  }
-
-
-
-  ])
-
-UserAudioPost.create!([
-  {
-    title: "Beats",
-    content: "sick beat",
-    user_id: 1,
-    audio_url: "www.beats.com",
-    tags: "Music"
-  },
-  {
-    title: "Bird calls",
-    content: "bird calls",
-    user_id: 2,
-    audio_url: "www.birds.com",
-    tags: "Nature"
-  },
-  {
-    title: "Thunder",
-    content: "scary thunder!",
-    user_id: 3,
-    audio_url: "www.Loundthunder.com",
-    tags: "Nature"
+    content: "This is a post about football",
+    title: "football",
+    post_type: "video"
   }
 
 
   ])
 
+MediaUrl.create([
+  {
+    post_id: 1,
+    url: "https://www.youtube.com/watch?v=bBH0ele7qdw"
+  },
+  {
+    post_id: 2,
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ807Vag4b7cBo9xl4zlKiNb-s-aFG8cocPyGQJRqNVbsIH5EZ1KQOz0w4N"
+  },
+  {
+    post_id: 4,
+    url: "https://www.youtube.com/watch?v=-lGrARvsuD0"
+  }
+
+  ])
 
 
 
