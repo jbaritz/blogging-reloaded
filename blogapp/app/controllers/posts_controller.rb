522057f19adf637ff99fc325d99941eb57dce07f
Julia Baritz <jbaritz@gmail.com>
2015-01-29 11:24:34 -0600

    forms work

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -7,6 +7,14 @@ class PostsController < ApplicationController
     #view  form
   end
 
+  def new_video_post
+    #view  form
+  end
+
+  def new_audio_post
+    #view  form
+  end
+
   def submit_text_post
     params['user_id'] = current_user.id
     # puts "PARAMS:" 
@@ -18,17 +26,43 @@ class PostsController < ApplicationController
   def submit_picture_post
     params['user_id'] = current_user.id
     UserPicturePost.create!(pic_params)
+    redirect_to "/"
+  end
+
+  def submit_video_post
+    params['user_id'] = current_user.id
+    UserVideoPost.create!(vid_params)
+    redirect_to "/"
+  end
+
+  def submit_audio_post
+    params['user_id'] = current_user.id
+    UserAudioPost.create!(audio_params)
+    redirect_to "/"
   end
 
   private
-  def text_params
-     params.require(:user_id)
-     params.permit(:title, :content, :tags, :user_id)
-  end
+    def text_params
+       params.require(:user_id)
+       params.permit(:title, :content, :tags, :user_id)
+    end
 
-  def pic_params
-     params.require(:user_id, :image_url)
-     params.permit(:title, :content, :image_url, :user_id)
-  end
+    def pic_params
+       params.require(:user_id)
+       params.require(:image_url)
+       params.permit(:title, :content, :image_url, :user_id, :tags)
+    end
+
+    def vid_params
+       params.require(:user_id)
+       params.require(:video_url)
+       params.permit(:title, :content, :video_url, :user_id, :tags)
+    end
+
+    def audio_params
+       params.require(:user_id)
+       params.require(:audio_url)
+       params.permit(:title, :content, :audio_url, :user_id, :tags)
+    end
 end
 
