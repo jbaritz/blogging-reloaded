67afb917f1712f3786b7cb7328e2a7a2db8fa588
Julia Baritz <jbaritz@gmail.com>
2015-01-29 09:24:19 -0600

    blah

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
   def new_text_post
+    #view  form
   end
 
   def submit_text_post
+    UserTextPost.new(text_params)
   end
+
+  private
+  def text_params
+     params.require(:post).permit(:title, :content, :tags)
+  end
+
 end
+
