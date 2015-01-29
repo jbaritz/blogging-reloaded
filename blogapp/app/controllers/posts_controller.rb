011104368d1284294b8478d0a42d32ba8081fe62
James Knepper <knepperjb@gmail.com>
2015-01-29 17:48:59 +0000

    Add blog post view

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -7,6 +7,17 @@ class PostsController < ApplicationController
     UserTextPost.new(text_params)
   end
 
+  def show_all_posts
+    @posts = []
+    @user = User.find_by(username: params[:username])
+    @texts = UserTextPost.where(user_id: @user.id)
+    @pics = UserPicturePost.where(user_id: @user.id)
+    @videos = UserVideoPost.where(user_id: @user.id)
+    @posts.push(@texts, @pics, @videos)
+    @posts.flatten!
+    @posts.sort_by! {|x| x.created_at }
+  end
+
   private
   def text_params
      params.require(:post).permit(:title, :content, :tags)
--- blogapp/app/models/user_text_post.rb
+++ blogapp/app/models/user_text_post.rb
@@ -1,3 +1,4 @@
 class UserTextPost < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, as: :media
 end
