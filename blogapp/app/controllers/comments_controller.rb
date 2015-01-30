ecf7660ec34a14dc60687cd1f36042f9ed0c90ce
Julia Baritz <jbaritz@gmail.com>
2015-01-30 15:27:23 -0600

    comments stuff

--- blogapp/app/controllers/comments_controller.rb
+++ blogapp/app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
-  def add_new_comment
-
-    comment = Comment.build_from(post, current_user.id, commenttext )
+  def submit_comment
+    post = Post.find(params[:id])
+    comment = Comment.build_from(post, current_user.id, params['body'])
     comment.save!
+    redirect_to "/posts/" + params[:id]
+  end
 end
