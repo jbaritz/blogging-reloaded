49cc2ef711d43d0e960306a59052540968937a0a
Julia Baritz <jbaritz@gmail.com>
2015-02-05 14:56:30 -0600

    reblog model changes

--- blogapp/app/models/reblog.rb
+++ blogapp/app/models/reblog.rb
@@ -1,7 +1,6 @@
 class Reblog < ActiveRecord::Base
   belongs_to :original_post
   belongs_to :user
-  belongs_to :user_blog_post, foreign_key: :post_id
 
  #you can just call "reblog.original_post" to grab all the post info for a reblog
 end
