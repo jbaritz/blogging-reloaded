f2659af2f74f2366a2f1b8e5080d155abe98c27d
Julia Baritz <jbaritz@gmail.com>
2015-02-05 15:26:23 -0600

    some post display and linking fixes

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:show, :show_all_posts]
   def show #this is okay
-    @post = UserBlogPost.find(params[:id]).get_post
+    @post = OriginalPost.find(params[:id])
     @comments = @post.comment_threads
     @comments_hash = @comments.map do |c|
       attrs = c.attributes
